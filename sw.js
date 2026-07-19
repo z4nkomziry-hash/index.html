7291ec5fce2549860777d84afa0b6735a7128967
z4nkomziry-hash <z4nkomziry@gmail.com>
2026-07-19 17:49:53 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'tikjet-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/icon.png',
+  '/profile.jpg',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
